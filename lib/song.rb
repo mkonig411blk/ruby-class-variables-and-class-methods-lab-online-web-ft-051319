6c42f2199422bc6b35a248de8b1b42094e1b5008
Margot Konig <margot.konig@blackrock.com>
2019-05-23 21:22:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,26 +12,33 @@ def initialize(name, artist, genre)
   @genre = genre
   @@count += 1
   @@genres << genre
-  @@artists << artist
+  @@artists << artist 
 end
 
 def self.count 
   @@count
 end
 
-def genres
-  @@genres
+def self.genres
+  @@genres.to_set
 end
 
 def self.artists
-  @@artists
+  @@artists.to_set
 end
 
-def self.genre_count
-  @@genres.each do |genre|
-    @@genres_count[genre] = @@genres.count[genre]
+  def self.genre_count
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      @@genre_count[genre] += 1 
+      else
+      @@genre_count[genre] = 1
+      end
+    end
+    @@genre_count
   end
-  @@genres_count
-end
 
+
+def self.artist_count
+end
 end
