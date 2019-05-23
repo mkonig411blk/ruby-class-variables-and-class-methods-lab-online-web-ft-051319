5d4ed9865f511ab4c09f2325457b65cdd3a91dc9
Margot Konig <margot.konig@blackrock.com>
2019-05-23 21:27:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,16 @@ end
     @@genre_count
   end
 
-
-def self.artist_count
+  
+    def self.artist_count
+    @@artists.each do |artist| 
+      if @@artists_count[artist]
+      @@artists_count[artist] += 1 
+      else
+      @@artists_count[artist] = 1
+      end
+    end
+    @@artists_count
+  end
 end
 end
